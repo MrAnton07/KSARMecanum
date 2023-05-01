81529db287dfba11bcde5f3e169f9522af1c6dac
Anton <MrAnton07@Mail.Ru>
2023-05-01 21:45:27 +0300

    [MEC-Koksharov](~) Temp changes

--- src/aruco_publisher/setup.py
+++ src/aruco_publisher/setup.py
@@ -20,7 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-              'ARUCO = aruco_publisher.aruco_detector:main',
+              'ARUCO1 = aruco_publisher.aruco_detector:main',
         ],
     },
 )
